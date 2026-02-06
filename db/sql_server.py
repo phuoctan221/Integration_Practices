8c57d781460eecc2d2025b09d366bc154fd701a8
phuoctan221 <phuoctan1802@gmail.com>
2026-02-06 10:01:43 +0700

    Cập nhật thêm ghi chú hướng dẫn sử dụng

--- db/sql_server.py
+++ db/sql_server.py
@@ -1,5 +1,7 @@
 import pyodbc
 
+# Sửa lại phù hợp với cấu hình SQL Server của từng bạn rồi test thử kết nối được chưa nhé
+
 def connect_sql_server():
     return pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
